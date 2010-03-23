1d948e279c86b6232c821bc895997830ded41502
John Bytheway <jbytheway@gmail.com>
2010-03-23 23:01:05 +0000

    Makefile structure and stub main()
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  std::cout <<
+"Subliminal, a subtitle extraction system\n"
+"Copyright (C) 2010 John Bytheway\n"
+"License GPLv3+: GNU GPL version 3 or later\n"
+"This is free software: you are free to change and redistribute it.\n"
+"There is NO WARRANTY, to the extent permitted by law." << std::endl;
+
+  return 0;
+}
+
