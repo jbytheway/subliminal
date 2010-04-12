50f9deed5738d9749314dc7d0673175219cba325
John Bytheway <jbytheway@gmail.com>
2010-04-12 22:37:49 +0100

    Made test data actually render some subtitles
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- subliminal/test/transcode.sh
+++ subliminal/test/transcode.sh
@@ -3,6 +3,12 @@
 set -e
 set -u
 
+function echo_and_run
+{
+  printf "%s\n" "$*"
+  "$@"
+}
+
 if [ $# != 1 ]
 then
   echo "Usage: $0 PARAMS" >&2
@@ -10,10 +16,10 @@ then
 fi
 
 PARAMS_FILE="$1"
-INPUT_FILE=root_video.avi
+INPUT_FILE=subbed_video.avi
 OUTPUT_FILE="${PARAMS_FILE%.params}.subbed"
 
 . "$PARAMS_FILE"
 
-mencoder $MENCODER_ARGS -o "$OUTPUT_FILE" "$INPUT_FILE"
+echo_and_run mencoder $MENCODER_ARGS -o "$OUTPUT_FILE" "$INPUT_FILE"
 
