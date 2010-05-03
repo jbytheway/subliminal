5a4cd08297f70f8e5315826b0c11ca53e1a3784b
John Bytheway <jbytheway@gmail.com>
2010-05-03 19:01:03 +0100

    Save accumulated images as pngs
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- subliminal/output.cpp
+++ subliminal/output.cpp
@@ -1,6 +1,7 @@
 #include "output.hpp"
 
 #include <boost/format.hpp>
+#include <boost/gil/extension/io/png_io.hpp>
 
 #include "fatal.hpp"
 
@@ -29,12 +30,13 @@ output::output(boost::filesystem::path const& root) :
 void output::save(
   boost::rational<int64_t> const& begin_time,
   boost::rational<int64_t> const& end_time,
-  boost::gil::rgb8c_view_t const& /*image*/
+  boost::gil::rgb8c_view_t const& image
 )
 {
   auto new_index = index_++;
   std::string uniq = (boost::format(".%04d") % new_index).str();
   auto image_name = root_.file_string()+uniq+".png";
+  boost::gil::png_write_view(image_name, image);
   txt_out_ << round_time(begin_time) << ' ' << round_time(end_time) << ' ' <<
     image_name << std::endl;
 }
