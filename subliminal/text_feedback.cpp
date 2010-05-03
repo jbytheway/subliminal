09920eae35af0766fcd36ccb305f0f5e362f0b33
John Bytheway <jbytheway@gmail.com>
2010-05-03 22:23:56 +0100

    Don't print from text_feedback on show()
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- subliminal/text_feedback.cpp
+++ subliminal/text_feedback.cpp
@@ -11,9 +11,8 @@ void text_feedback::message(std::string const& m)
   out_ << m << std::endl;
 }
 
-void text_feedback::show_rgb(boost::gil::rgb8c_view_t const&, int image)
+void text_feedback::show_rgb(boost::gil::rgb8c_view_t const&, int /*image*/)
 {
-  out_ << "\n[frame " << image << "]" << std::endl;
 }
 
 void text_feedback::progress(int pos, int outof)
